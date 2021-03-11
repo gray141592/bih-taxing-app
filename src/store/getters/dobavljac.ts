c73af5d28c7e36630db91560d6d2878b63007356
gray.141592 <gray.141592@gmail.com>
2021-03-11 02:56:31 +0100

    popravka pretrage nabavljaca

--- src/store/getters/dobavljac.ts
+++ src/store/getters/dobavljac.ts
@@ -4,8 +4,11 @@ import { PdvObveznik } from '@/models/pdv';
 // eslint-disable-next-line max-len
 const dobavljac = (state: State) => (id: string): PdvObveznik | undefined => {
   const { dobavljaci } = state;
-  const nasao = dobavljaci.find((obveznik) => [obveznik.jib, obveznik.pdvBroj].includes(id));
-  return nasao;
+  if (!id) {
+    return;
+  }
+  // eslint-disable-next-line consistent-return
+  return dobavljaci.find((obveznik) => [obveznik.jib, obveznik.pdvBroj].includes(id));
 };
 
 export default dobavljac;
